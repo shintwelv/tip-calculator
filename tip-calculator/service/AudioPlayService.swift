7d69a50fa42b04b1f08395bff51f495b9d7b2d8d
elvinHeo <shintwl@icloud.com>
2024-03-12 22:56:37 +0900

    added gesture

---
+++ tip-calculator/service/AudioPlayService.swift
@@ -0,0 +1,29 @@
+//
+//  AudioPlayService.swift
+//  tip-calculator
+//
+//  Created by ShinIl Heo on 3/12/24.
+//
+
+import Foundation
+import AVFoundation
+
+protocol AudioPlayerService {
+    func playSound()
+}
+
+final class DefaultAudioPlayer: AudioPlayerService {
+    
+    private var player: AVAudioPlayer?
+    
+    func playSound() {
+        let path = Bundle.main.path(forResource: "click", ofType: "m4a")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
--- tip-calculator/view model/CalculatorVM.swift
+++ tip-calculator/view model/CalculatorVM.swift
@@ -14,13 +14,19 @@ class CalculatorVM {
         let billPublisher: AnyPublisher<Double, Never>
         let tipPublisher: AnyPublisher<Tip, Never>
         let splitPublisher: AnyPublisher<Int, Never>
+        let logoViewTapPublisher: AnyPublisher<Void, Never>
     }
     
     struct Output {
         let updateViewPublisher: AnyPublisher<Result, Never>
+        let resetCalculatorPublisher: AnyPublisher<Void, Never>
     }
     
     private var cancellables = Set<AnyCancellable>()
+    private let audioPlayerService: AudioPlayerService
+    init(audioPlayerService: AudioPlayerService = DefaultAudioPlayer()) {
+        self.audioPlayerService = audioPlayerService
+    }
     
     func transform(input: Input) -> Output {
         let updateViewPublisher = Publishers.CombineLatest3(
@@ -36,7 +42,18 @@ class CalculatorVM {
                     totalTip: totalTip)
                 return Just(result)
             }.eraseToAnyPublisher()
-        return Output(updateViewPublisher: updateViewPublisher)
+        
+        let resetCalculatorPublisher = input
+            .logoViewTapPublisher
+            .handleEvents(receiveOutput: { [unowned self] _ in
+                audioPlayerService.playSound()
+            }).flatMap {
+                return Just($0)
+            }.eraseToAnyPublisher()
+        
+        return Output(
+            updateViewPublisher: updateViewPublisher,
+            resetCalculatorPublisher: resetCalculatorPublisher)
     }
     
     private func getTipAmount(bill: Double, tip: Tip) -> Double {
