97a4ad3c6036889ceca7510fdb29a8ba37b11b82
elvinHeo <shintwl@icloud.com>
2024-03-12 23:02:57 +0900

    completed reset functionality

--- tip-calculator/CalculatorVC.swift
+++ tip-calculator/CalculatorVC.swift
@@ -46,12 +46,12 @@ class CalculatorVC: UIViewController {
     private lazy var logoViewTapPublisher: AnyPublisher<Void, Never> = {
         let tapGesture = UITapGestureRecognizer(target: self, action: nil)
         tapGesture.numberOfTapsRequired = 2
-        view.addGestureRecognizer(tapGesture)
+        logoView.addGestureRecognizer(tapGesture)
         return tapGesture.tapPublisher.flatMap { _ in
             Just(())
         }.eraseToAnyPublisher()
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
@@ -73,8 +73,23 @@ class CalculatorVC: UIViewController {
             resultView.configure(result: result)
         }.store(in: &cancellables)
         
-        output.resetCalculatorPublisher.sink { _ in
-            print("hey reset the form please!")
+        output.resetCalculatorPublisher.sink { [unowned self] _ in
+            billInputView.reset()
+            tipInputView.reset()
+            splitInputView.reset()
+            
+            UIView.animate(
+                withDuration: 0.1,
+                delay: 0,
+                usingSpringWithDamping: 5.0,
+                initialSpringVelocity: 0.5,
+                options: .curveEaseInOut) {
+                    self.logoView.transform = .init(scaleX: 1.5, y: 1.5)
+                } completion: { _ in
+                    UIView.animate(withDuration: 0.1) {
+                        self.logoView.transform = .identity
+                    }
+                }
         }.store(in: &cancellables)
     }
     
--- tip-calculator/view/SplitInputView.swift
+++ tip-calculator/view/SplitInputView.swift
@@ -81,6 +81,10 @@ class SplitInputView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func reset() {
+        splitSubject.send(1)
+    }
+    
     private func layout() {
         
         [headerView, stackView].forEach(addSubview(_:))
